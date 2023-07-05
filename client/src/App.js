ae0272c7b5ecec5f594034f6c1f12fc25e660779
soumali28 <gorai.soumali@gmail.com>
2023-07-05 23:14:02 +0530

    setting up tailwind and daisy ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import React from "react";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<Home />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
