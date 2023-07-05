4530a15388131df847e656b343ae6647d253e3ca
Rup28 <rupkatharay62737@gmail.com>
2023-07-05 22:21:23 +0530

    route change

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home/>} />
+      <Route path="/" element={<Home/>} />
     </Routes>
   )
 }
